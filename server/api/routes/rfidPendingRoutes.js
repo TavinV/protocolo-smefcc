086116a02ae948eb26cf2af211d51cc6ecea6db0
Otávio Vinícius Flauzino <159206555+TavinV@users.noreply.github.com>
2025-11-28 15:34:26 -0300

    Change POST route authentication to use X-API-Key

--- server/api/routes/rfidPendingRoutes.js
+++ server/api/routes/rfidPendingRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import rfidPendingController from '../controllers/rfidPendingController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
+import { authenticateXApiKey } from '../middleware/authXApiKey.js';
 
 const router = express.Router();
 
 router.get('/', authenticateToken, rfidPendingController.getAll);
-router.post('/', authenticateToken, rfidPendingController.create);
+router.post('/', authenticateXApiKey, rfidPendingController.create);
 router.delete('/:id', authenticateToken, rfidPendingController.remove);
 
 export default router;
